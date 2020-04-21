4fe0a9f3c41f9dc12bc3200f25ad3a7cacfebb22
Leonardo Lira <leonardo.lira@hotmail.com.br>
2020-04-21 17:26:19 -0300

    test: ensure SignUpController return an error if no email is provider

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,3 +16,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   });
 });
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'test',
+        password: '123',
+        passwordConfirm: '123',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+  });
+});
