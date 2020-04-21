52c9cf2cc01c093e5c0e4103d675edf6246cb4bb
Leonardo Lira <leonardo.lira@hotmail.com.br>
2020-04-21 17:01:13 -0300

    test: ensure SignUpContrller returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup';
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: 'teste@test.com',
+        password: '123',
+        passwordConfirm: '123',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
