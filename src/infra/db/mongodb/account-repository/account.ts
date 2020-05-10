5c36512a5877bed47b54da59cc87fc0cdcb1619c
Leonardo Lira <leonardo.lira@hotmail.com.br>
2020-05-10 19:26:07 -0300

    feat: ensure AccountMongoRepository return an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,18 @@
+import { IAddAccountRepository } from '../../../../data/protocols/add-account-repository';
+import { IAddAccountModel } from '../../../../domain/usecases/add-account';
+import { IAccountModel } from '../../../../domain/models/account';
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class AccountMongoRepository implements IAddAccountRepository {
+  async add(accountData: IAddAccountModel): Promise<IAccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    const result = await accountCollection.insertOne(accountData);
+    const account = result.ops[0];
+    const { _id, ...accountWithOutId } = account;
+
+    return {
+      id: _id,
+      ...accountWithOutId,
+    };
+  }
+}
