9b0983f9bdb72e2b109c3a27f5b1be91ab9bd305
Leonardo Lira <leonardo.lira@hotmail.com.br>
2020-05-27 22:28:20 -0300

    test: ensure SignUpController return badRequest when Validation fails

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -273,4 +273,21 @@ describe('SignUp Controller', () => {
     await sut.handle(httpRequest);
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
   });
+
+  test('Should return badrequest when validate fails', async () => {
+    const { sut, validationStub } = makeSut();
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'));
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@test.com',
+        password: '123',
+        passwordConfirm: '123',
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')));
+  });
 });
