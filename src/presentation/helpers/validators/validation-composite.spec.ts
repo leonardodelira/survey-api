c8ccc28ddf1f00af41a8221a7bb990dff16b2805
Leonardo Lira <leonardo.lira@hotmail.com.br>
2020-06-13 19:11:37 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,16 +2,36 @@ import { ValidationComposite } from './validation-composite';
 import { MissingParamError } from '../../errors';
 import { IValidation } from './validation';
 
+interface SutTypes {
+  sut: ValidationComposite;
+  validationStub: IValidation;
+}
+
+const makeValidationStub = (): IValidation => {
+  class ValidationStub implements IValidation {
+    validate(input: any): Error {
+      return null;
+    }
+  }
+  return new ValidationStub();
+};
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub();
+  const sut = new ValidationComposite([validationStub]);
+
+  return {
+    validationStub,
+    sut,
+  };
+};
+
 describe('Validation Composite', () => {
   test('Should return an error if any validators fail', () => {
-    class ValidationStub implements IValidation {
-      validate(input: any): Error {
-        return new MissingParamError('email');
-      }
-    }
-    const validationStub = new ValidationStub();
-    const sut = new ValidationComposite([validationStub]);
+    const { sut, validationStub } = makeSut();
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('email'));
     const error = sut.validate({ name: 'any_name' });
+
     expect(error).toEqual(new MissingParamError('email'));
   });
 });
