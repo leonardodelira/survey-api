444a7c8627e766865f2b6fbafdba0cf041fc2147
Leonardo Lira <leonardo.lira@hotmail.com.br>
2020-06-13 18:51:26 -0300

    test: return InvalidaParamError if compare fails/not return InvalidaParamError if compare success

---
+++ src/presentation/helpers/validators/compare-field-validation.spec.ts
@@ -0,0 +1,20 @@
+import { CompareFieldsValidation } from './compare-fields-validation';
+import { InvalidParamError } from '../../errors';
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('password', 'confirm_password');
+};
+
+describe('Required field compare', () => {
+  test('Should return a InvalidParamError if compare fails', () => {
+    const sut = makeSut();
+    const error = sut.validate({ password: 'any_pass', confirm_password: 'wrong_pass' });
+    expect(error).toEqual(new InvalidParamError('confirm_password'));
+  });
+
+  test('Should not return a InvalidParamError if compare success', () => {
+    const sut = makeSut();
+    const error = sut.validate({ password: 'any_pass', confirm_password: 'any_pass' });
+    expect(error).toBeFalsy();
+  });
+});
