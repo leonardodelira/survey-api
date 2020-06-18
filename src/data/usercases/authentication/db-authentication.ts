9d07044f1d65f4480a6f2bb8ea1cfc19953b3692
Leonardo Lira <leonardo.lira@hotmail.com.br>
2020-06-17 21:37:40 -0300

    feat: ensure DbAuthentication call LoadAccoutnByEmailRepository with a correct email

---
+++ src/data/usercases/authentication/db-authentication.ts
@@ -0,0 +1,15 @@
+import { IAuthentication, IAuthenticationModel } from '../../../domain/usecases/authentication';
+import { ILoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository';
+
+export class DbAuthentication implements IAuthentication {
+  private readonly loadAccountByEmailRepositoryStub: ILoadAccountByEmailRepository;
+
+  constructor(loadAccountByEmailRepositoryStub: ILoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepositoryStub = loadAccountByEmailRepositoryStub;
+  }
+
+  async auth(authentication: IAuthenticationModel): Promise<string> {
+    await this.loadAccountByEmailRepositoryStub.load(authentication.email);
+    return '';
+  }
+}
