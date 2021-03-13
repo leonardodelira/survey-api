24e7449e31fbe31dcf5b5c33548503c360bb612f
leonardodelira <leonardo.lira@hotmail.com.br>
2021-03-13 13:06:11 -0300

    feat: ensure JwtAdapter retuns a decrypted value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -15,8 +15,8 @@ export class JwtAdapter implements ITokenGenerator, IDecrypter {
     return accessToken;
   }
 
-  async decrypt(value: string): Promise<string> {
-    await jwt.verify(value, this.secret)
-    return await new Promise(resolve => resolve(null))
+  async decrypt(token: string): Promise<string> {
+    const value: any = await jwt.verify(token, this.secret)
+    return value;
   }
 }
