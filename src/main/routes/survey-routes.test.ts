d86b80133abdee3e0c4e668c49bb34335edb86a0
leonardodelira <leonardo.lira@hotmail.com.br>
2021-03-13 20:20:16 -0300

    test: ensure route /surveys return 403 if accessToken not provide

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -20,7 +20,7 @@ describe('Login Routes', () => {
   });
 
   describe('POST /surveys', () => {
-    test('Should return 204 on add survey success', async () => {
+    test('Should return 403 on add survey without accessToken', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -33,7 +33,7 @@ describe('Login Routes', () => {
             answer: 'Answer 1',
           }]
         })
-        .expect(204);
+        .expect(403);
     });
   });
 });
