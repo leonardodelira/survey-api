b9ee33f7c399907da8accdccfa524d2e81a9428c
leonardodelira <leonardo.lira@hotmail.com.br>
2021-03-13 19:25:08 -0300

    feat: ensure AccountMongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -4,8 +4,9 @@ import { IAccountModel } from '../../../../domain/models/account';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { ILoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository';
 import { IUpdateAccessTokenRepository } from '../../../../data/protocols/db/account/update-access-token-repository';
+import { ILoadAccountByTokenRepository } from '../../../../data/protocols/db/account/load-account-by-token-repository';
 
-export class AccountMongoRepository implements IAddAccountRepository, ILoadAccountByEmailRepository, IUpdateAccessTokenRepository {
+export class AccountMongoRepository implements IAddAccountRepository, ILoadAccountByEmailRepository, IUpdateAccessTokenRepository, ILoadAccountByTokenRepository {
   async add(accountData: IAddAccountModel): Promise<IAccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData);
@@ -33,5 +34,12 @@ export class AccountMongoRepository implements IAddAccountRepository, ILoadAccou
         },
       }
     );
+  }
+
+  async loadByToken(token: string, role?: string): Promise<IAccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const account = await accountCollection.findOne({ accessToken: token, role });
+
+    return MongoHelper.map(account);
   }
 }
