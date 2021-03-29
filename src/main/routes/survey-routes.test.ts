7c252d94743aacf20178f4b118d534a0706571b6
leonardodelira <leonardo.lira@hotmail.com.br>
2021-03-29 14:36:11 -0300

    test: ensure load surveys route return 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -25,6 +25,14 @@ describe('Login Routes', () => {
     await accountCollection.deleteMany({});
   });
 
+  describe('GET /surveys', () => {
+    test('Should return 403 on load surveys without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403);
+    });
+  });
+
   describe('POST /surveys', () => {
     test('Should return 403 on add survey without accessToken', async () => {
       await request(app)
