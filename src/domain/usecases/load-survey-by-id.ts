cfbf365ff7e5388ecec228bee301319e8dc6d83b
leonardodelira <leonardo.lira@hotmail.com.br>
2021-04-14 17:47:21 -0300

    feat: ensure DbLoadSurveyById call LoadSurveyByRepository with correct values

--- src/domain/usecases/load-survey-by-id.ts
+++ src/domain/usecases/load-survey-by-id.ts
@@ -1,5 +1,5 @@
 import { ISurveyModel } from '../models/survey';
 
-export interface ILoadSurveys {
-  load(): Promise<ISurveyModel[]>;
+export interface ILoadSurveyById {
+  loadById(id: string): Promise<ISurveyModel>;
 }
