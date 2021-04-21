0f371bd65e419905c579151c3fb0cae11577b963
leonardodelira <leonardo.lira@hotmail.com.br>
2021-04-21 12:28:00 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult

---
+++ src/infra/db/mongodb/survey/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,28 @@
+import { ISaveSurveyResultRepository } from '../../../../../data/protocols/db/survey/save-survey-result-repository';
+import { ISurveyResultModel } from '../../../../../domain/models/survey-result';
+import { ISaveSurveyResultModel } from '../../../../../domain/usecases/save-survey-result';
+import { MongoHelper } from '../../helpers/mongo-helper';
+
+export class SurveyResultMongoRepository implements ISaveSurveyResultRepository {
+  async save(survey: ISaveSurveyResultModel): Promise<ISurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults');
+    const res = await surveyResultCollection
+      .findOneAndUpdate(
+        {
+          surveyId: survey.surveyId,
+          accountId: survey.accountId
+        },
+        {
+          $set: {
+            answer: survey.answer,
+            date: survey.date
+          }
+        },
+        {
+          upsert: true,
+          returnOriginal: false,
+        });
+
+    return res.value && MongoHelper.map(res.value);
+  }
+}
