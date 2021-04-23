c1045f32d1e9e5312c38d639d9c8baf7220c8866
leonardodelira <leonardo.lira@hotmail.com.br>
2021-04-23 10:25:26 -0300

    test: ensure save survey result retuns 200 if token is provided

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,6 +1,32 @@
 import request from 'supertest';
 import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
+import { Collection } from 'mongodb';
+import { sign } from 'jsonwebtoken';
+import env from '../config/env';
+
+let surveyCollection: Collection;
+let accountCollection: Collection;
+
+const makeAccesssToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Leonardo',
+    email: 'leonardo.lira@hotmail.com',
+    password: '123'
+  });
+
+  const id = res.ops[0]._id;
+  const accessToken = sign({ id }, env.jwt)
+  await accountCollection.updateOne({
+    _id: id,
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+
+  return accessToken
+}
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -11,6 +37,14 @@ describe('Login Routes', () => {
     await MongoHelper.disconnect();
   });
 
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    await surveyCollection.deleteMany({});
+
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
   describe('PUT /surveys/:surveyId/results', () => {
     test('Should return 403 on save survey result without accessToken', async () => {
       await request(app)
@@ -19,6 +53,26 @@ describe('Login Routes', () => {
           answer: 'any_answer'
         })
         .expect(403);
+    });
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const accessToken = await makeAccesssToken();
+      const res = await surveyCollection.insertOne({
+        question: 'Question 1',
+        answers: [{
+          answer: 'any_answer',
+          image: 'http://image-name.com'
+        }],
+        date: new Date()
+      })
+      const id = res.ops[0]._id;
+      await request(app)
+        .put(`/api/surveys/${id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(200);
     });
   });
 });
