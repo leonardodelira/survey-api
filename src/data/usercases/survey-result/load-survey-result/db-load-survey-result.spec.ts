69f011801286b3b8b8e4dae8df5370d2d25dab66
leonardodelira <leonardo.lira@hotmail.com.br>
2021-05-22 19:54:12 -0300

    refactor: move sut creation to a factory method

--- src/data/usercases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usercases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,19 +1,35 @@
 import { ILoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository';
 import { ISurveyResultModel } from '@/domain/models/survey-result';
 import { mockFakeSurveyResult } from '@/domain/test';
-import { ILoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result';
 import { DbLoadSurveyResult } from './db-load-survey-result';
 
 describe('DbLoadSurveyResult UseCase', () => {
-  class LoadSurveyResultRepositoryStub implements ILoadSurveyResultRepository {
-    async loadBySurveyId(surveyId: string): Promise<ISurveyResultModel> {
-      return Promise.resolve(mockFakeSurveyResult());
-    }
+  interface SutTypes {
+    sut: DbLoadSurveyResult;
+    loadSurveyResultRepositoryStub: ILoadSurveyResultRepository;
   }
-  const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub();
+
+  const mockLoadSurveyResultRepositoryStub = () => {
+    class LoadSurveyResultRepositoryStub implements ILoadSurveyResultRepository {
+      async loadBySurveyId(surveyId: string): Promise<ISurveyResultModel> {
+        return Promise.resolve(mockFakeSurveyResult());
+      }
+    }
+    return new LoadSurveyResultRepositoryStub();
+  };
+
+  const makeSut = (): SutTypes => {
+    const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepositoryStub();
+    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub);
+
+    return {
+      sut,
+      loadSurveyResultRepositoryStub,
+    };
+  };
 
   test('Should call LoadSurveyResultRepository', async () => {
-    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub);
+    const { sut, loadSurveyResultRepositoryStub } = makeSut();
     const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId');
     await sut.load('any_survey_id');
     expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id');
