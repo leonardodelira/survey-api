3bd195482982b5cdb22f29aef201397ff28d1dd7
leonardodelira <leonardo.lira@hotmail.com.br>
2021-05-26 20:42:20 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyByIdRepo if LoadSurveyResultRepo returns null

--- src/data/usercases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usercases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -1,12 +1,16 @@
 import { ILoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository';
+import { ILoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository';
 import { ISurveyResultModel } from '@/domain/models/survey-result';
 import { ILoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result';
 
 export class DbLoadSurveyResult implements ILoadSurveyResult {
-  constructor(private readonly loadSurveyResultRepository: ILoadSurveyResultRepository) {}
+  constructor(private readonly loadSurveyResultRepository: ILoadSurveyResultRepository, private readonly loadSurveyByIdRepository: ILoadSurveyByIdRepository) {}
 
   async load(surveyId: string): Promise<ISurveyResultModel> {
     const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId);
+    if (!surveyResult) {
+      await this.loadSurveyByIdRepository.loadById(surveyId);
+    }
     return surveyResult;
   }
 }
